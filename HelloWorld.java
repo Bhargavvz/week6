d6b20324fa580e201b41efe5044430e0559d48f4
Bhargav Adepu <adepuvaatsavasribhargav@gmail.com>
2024-09-24 14:01:28 +0530

    added

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    // Main method - entry point of the program
+    public static void main(String[] args) {
+        // Create an instance of the class
+        HelloWorld hello = new HelloWorld();
+        // Call the greet method
+        hello.greet("World");
+    }
+
+    // Method to greet a person
+    public void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
